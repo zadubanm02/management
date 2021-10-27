515e77e901939d5229d3b2ac352ee398f7a3815d
Michal Zaduban <51057141+zadubanm02@users.noreply.github.com>
2021-10-27 14:59:52 +0200

    Update index.ts

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,3 +1,3 @@
-export const MONGOURI = "mongodb+srv://user:user@cluster0.27rnu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const MONGOURI = "uri"
 export const APP_SECRET = "APP_SECRET"
-export const REDISURI = "redis-12935.c250.eu-central-1-1.ec2.cloud.redislabs.com"
+export const REDISURI = ""

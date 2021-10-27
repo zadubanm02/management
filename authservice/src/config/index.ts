050e3b429f2bc49ca634fe5e7c5333a008dca10b
Michal Zaduban <51057141+zadubanm02@users.noreply.github.com>
2021-10-27 14:59:18 +0200

    Update index.ts

--- authservice/src/config/index.ts
+++ authservice/src/config/index.ts
@@ -1,2 +1,2 @@
-export const MONGOURI = "mongodb+srv://user:user@cluster0.vq9z8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const MONGOURI = "uri"
 export const APP_SECRET = "APP_SECRET"
